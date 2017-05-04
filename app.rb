633c4caa42e2a1dd6060412715fce1abf23d63f5
Jason Ainsworth and James Higgins <james@>
2017-05-04 16:48:01 -0700

    add in app.rb files and functionality with part of sinatra working. lots still broken

--- app.rb
+++ app.rb
@@ -9,28 +9,50 @@ also_reload('lib/**/*.rb')
 DB = PG.connect({:dbname => 'library'})
 
 get('/') do
+  @persons = Person.all
+  @books = Book.all
   erb(:index)
 end
 
 get('/book/new') do
-  erb(:add_book_form)
+  @books = Book.all
+  erb(:books)
 end
 
 post('/book/new') do
   title = params.fetch("title")
   author = params.fetch("author")
   attributes = {:title => title, :author => author}
-  book = Book.new(attributes).save
-  @books = Book.all
-  erb(:index)
-end
-
-get('/books') do
+  Book.new(attributes).save
+  @persons = Person.all
   @books = Book.all
   erb(:books)
 end
 
+get('/person/new') do
+  @persons = Person.all
+  erb(:persons)
+end
+
+post('/person/new') do
+  name = params.fetch('name')
+  @person = Person.new({:name => name}).save
+  @persons = Person.all
+  erb(:persons)
+end
+
+
 get('/book/:id') do
   @book = Book.find(params.fetch("id").to_i)
-  erb(:book)
+  @persons = Person.all
+  erb(:book_info)
+end
+
+get('/person/:id') do
+  @person = Person.find(params.fetch('id').to_i)
+  @books = Book.all
+  @books.each do |book|
+    @book = book
+  end
+  erb(:person_info)
 end
